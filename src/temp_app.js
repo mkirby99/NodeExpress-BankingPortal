77fa35b5520513327ee15250057af53c04527d41
Matthew Kirby <matthew.kirby3@fidelity.com>
2022-07-15 15:55:34 -0400

    comments out

--- src/temp_app.js
+++ src/temp_app.js
@@ -4,15 +4,15 @@ const express = require('express');
 
 const app = express();
 
-const { accounts, users, writeJSON } = require('./data');
-const accountRoutes = require('./routes/accounts');
-const servicesRoutes = require('./routes/services');
+// const { accounts, users, writeJSON } = require('./data');
+// const accountRoutes = require('./routes/accounts');
+// const servicesRoutes = require('./routes/services');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
@@ -20,4 +20,4 @@ app.use('/account', accountRoutes);
 app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
-app.listen(3000, () => console.log('PS project running on port 3000!'));
+app.listen(3000);
