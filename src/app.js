48f88342c4b3b68d4b9ffeebf8beded486549367
Matthew Kirby <matthew.kirby3@fidelity.com>
2022-07-15 16:47:38 -0400

    fix1

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
-
-const app = express();
-
-const { accounts, users, writeJSON } = require('./data');
+const {accounts, users, writeJSON} = require('./data');
 const accountRoutes = require('./routes/accounts');
 const servicesRoutes = require('./routes/services');
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+app.use(express.urlencoded({ extended: true}));
 
 app.use('/account', accountRoutes);
 app.use('/services', servicesRoutes);
 
-app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
-app.listen(3000, () => console.log('PS project running on port 3000!'));
+
+app.get('/', (req, res) => res.render('index', {title:'Account Summary', accounts}));
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {user:users[0]});
+})
+
+
+
+
+app.listen(3000, ()=> console.log('Ps project running on port 3000'));
--- src/temp_app.js
+++ src/temp_app.js
@@ -4,15 +4,15 @@ const express = require('express');
 
 const app = express();
 
-// const { accounts, users, writeJSON } = require('./data');
-// const accountRoutes = require('./routes/accounts');
-// const servicesRoutes = require('./routes/services');
+const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
